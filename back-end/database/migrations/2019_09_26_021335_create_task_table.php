59e7ef64d0b13411ebf60a4043072ca88a1552e8
JDR <42478354+jdr81394@users.noreply.github.com>
2019-09-25 22:47:59 -0400

    set up backend env, configured packages, set up middlware for cors, created migration, created controller, got front end hitting back end, set up database connection. successfully hitting db and bringing back object. migration was successful. as well

---
+++ back-end/database/migrations/2019_09_26_021335_create_task_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(!Schema::connection('mysql')->hasTable('tasks')) {
+            Schema::connection('mysql')->create('tasks', function(Blueprint $table) {
+                $table->increments('id');
+                $table->tinyInteger('importance')->unsigned()->nullable();
+                $table->tinyInteger('urgency')->unsigned()->nullable();
+                $table->enum('time', ['Quick', 'Long', 'Very Long']);
+                $table->timestamps();
+            });
+        } 
+        // Schema::table('tasks', function (Blueprint $table) {
+        //     //
+        // });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('mysql')->dropIfExists('tasks');
+    }
+}
