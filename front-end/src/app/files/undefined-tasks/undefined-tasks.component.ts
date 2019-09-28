c876ed40b194a11aea85240982cdd89f7aaf243b
JDR <42478354+jdr81394@users.noreply.github.com>
2019-09-27 22:59:47 -0400

    got working in front end

--- front-end/src/app/files/undefined-tasks/undefined-tasks.component.ts
+++ front-end/src/app/files/undefined-tasks/undefined-tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 /** CLASSES */
 import { Task } from '../../classes/task/task';
@@ -13,11 +14,39 @@ import { TaskService } from '../../services/task-service/task.service';
 })
 export class UndefinedTasksComponent implements OnInit {
 
-  todo: Task[];
-  constructor(private taskService: TaskService) { }
+  // todo: Task[];
+  todo: any[];
+  done: any[];
+  constructor(private taskService: TaskService) { 
+    // this.todo = [
+    //   'Get to work',
+    //   'Pick up groceries',
+    //   'Go home',
+    //   'Fall asleep'
+    // ];
+  
+    this.done = [
+      'Get up',
+      'Brush teeth',
+      'Take a shower',
+      'Check e-mail',
+      'Walk dog'
+    ];
+  }
 
   ngOnInit() {
     this.taskService.getUndefinedTasks().subscribe( (res: Task[]) => this.todo = res );
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
   }
 
 
